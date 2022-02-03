34d6b83f175710fa464665fc8d165b172c9e64b3
nayab fatima <syedanayab2014@hotmail.com>
2022-02-03 12:55:50 +0100

    initial

---
+++ sand.js
@@ -0,0 +1,38 @@
+
+// var fullName = firstName + lastName;
+
+// console.log(fullName);
+
+
+// console.log(2+2);
+
+// let radius = 10;
+// let result = radius * 3;
+// console.log(result);
+
+// let apples = 20;
+// let result = "i have" +  " " apples  " " + "dolls";
+
+// console.log(result);
+
+// let result = `${firstName} ${lastName} has ${apples} pens`;
+// console.log(result);
+
+let nayab = ['dua', 'irfan', 'happyl life'];
+// let result = nayab.join('*');console.log(nayab.length);
+console.log(nayab.length);
+
+hvjbjdbjbvms
+
+  // result = nayab.concat(['love', 'love', 'love']);
+  // console.log(result);console.log(nayab.length);
+  console.log(nayab.length);
+
+
+  // let result =  nayab.push('beta');
+  // console.log(result);
+
+  // console.log(nayab);
+ result = nayab.pop();
+  console.log(result);
+  console.log(nayab);
